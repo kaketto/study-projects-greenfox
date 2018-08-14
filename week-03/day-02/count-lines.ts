61689371b862a536f13c8bebad2418f57d74761d
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-14 14:58:12 +0200

    count lines exercise completed

---
+++ week-03/day-02/count-lines.ts
@@ -0,0 +1,21 @@
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+'use strict';
+
+const fs = require('fs');
+
+function numberOfLines(file:string) {
+  try {
+    let fileContentArray: string[] = fs.readFileSync(file, 'utf-8').split('\n');
+    return fileContentArray.length;
+  } catch (e) {
+      return 0;
+  }
+}
+
+console.log(numberOfLines('my-file.txt'));
+console.log(numberOfLines('my-files.txt'));
+
