30a8576792833240c81c2d7ce5b7d55e25c6aa03
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-14 15:21:36 +0200

    write single line exercise completed

---
+++ week-03/day-02/write-single-line.ts
@@ -0,0 +1,19 @@
+// Open a file called 'my-file.txt'
+// Write your name in it as a single line
+// If the program is unable to write the file,
+// then it should print an error message like: 'Unable to write file: my-file.txt'
+
+'use strict';
+
+const fs = require('fs');
+
+function writeAndReadMyName(fileName: string, myName: string) {
+  try {
+    fs.writeFileSync(fileName, myName);
+  } catch (e) {
+    throw new Error(`Unable to write file: ${fileName}`)
+  }
+  return fs.readFileSync(fileName, 'utf-8');
+}
+
+console.log(writeAndReadMyName('my-file.txt', 'Kinga Moldovanyi'));
