d872126069c86a6bc1d8e33ef0d00e32ca3b9b1a
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-16 12:35:52 +0200

    (feat)count weird bunnies' ears function is completed

---
+++ week-03/day-04/bunny2.ts
@@ -0,0 +1,17 @@
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+'use strict';
+
+function numberOfEars(numberOfBunnies: number) {
+  if (numberOfBunnies === 1) {
+    return 2;
+  } else if (numberOfBunnies % 2 === 0 ) {
+    return 3 + numberOfEars(numberOfBunnies - 1);
+  } else {
+    return 2 + numberOfEars(numberOfBunnies - 1);
+  }
+}
+
+console.log(numberOfEars(7));
