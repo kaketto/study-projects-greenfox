6edc8a9c2d9247a196f74a2584abe1b1b4a4589b
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-16 11:48:48 +0200

    (feat): rainbow box function with recursion

--- week-02/day-03/rainbow-box-function/rainbow-box-function-ts.ts
+++ week-02/day-03/rainbow-box-function/rainbow-box-function-ts.ts
@@ -11,27 +11,33 @@ const ctx = canvas.getContext('2d');
 // Create a loop that fills the canvas with rainbow colored squares.
 
 let rainbowColors: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-function drawRainbowSquares (size: number, color: any) {
+
+function drawRainbowSquares (size: number, color: string) {
     ctx.fillStyle = color;
     ctx.fillRect((canvas.width - size) / 2, (canvas.height - size) / 2, size, size); 
 }
 
-/* RANDOM SIZE SQUARES
-
+/* RANDOM SIZE SQUARES WITH RANDOM COLORS
 for (let i: number = canvas.height / 2; i > 0; i--) {
     let rgbRandom = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
     drawRainbowSquares(Math.floor(Math.random() * canvas.height), rgbRandom);
 }
 */
 
-// RANDOM BUT FIXED DISTANCE SQUARES
+// RANDOM BUT FIXED DISTANCE SQUARES WITH RAINBOW COLORS
+// let sizeDiff: number = Math.floor(Math.random() * 100);
+// let squareSize: number = canvas.height;
+// for (let i: number = 0; i < canvas.height / sizeDiff; i++) {
+//     drawRainbowSquares(squareSize, rainbowColors[i % rainbowColors.length]);
+//     squareSize -= sizeDiff;
+// }
 
-let sizeDiff: number = Math.floor(Math.random() * 100);
-let squareSize: number = canvas.height;
-for (let i: number = 0; i < canvas.height / sizeDiff; i++) {
-    drawRainbowSquares(squareSize, rainbowColors[i % rainbowColors.length]);
-    squareSize -= sizeDiff;
+//RECURSIVE SOLUTION WITH DYNAMIC DIFFERENT SIZES
+function recursiveSquare (size: number, difference: number, colorIndex: number) {
+    if (size > 10) {
+        drawRainbowSquares(size, rainbowColors[colorIndex]);
+        recursiveSquare(size - difference, difference, (colorIndex + 1) % rainbowColors.length);
+    }
 }
 
-
-
+recursiveSquare(canvas.height, 50, 0);
