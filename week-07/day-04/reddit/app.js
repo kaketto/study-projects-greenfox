5b596678e662a575fb877d0f42293074743d948a
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-09-20 12:03:15 +0200

    add(reddit): endpoint to list all posts

--- week-07/day-04/reddit/app.js
+++ week-07/day-04/reddit/app.js
@@ -9,19 +9,19 @@ const mysql = require('mysql');
 
 app.use('/assets', express.static('assets'));
 
-// const connection = mysql.createConnection ({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'pw1234',
-//   database: 'reddit',
-// });
-// connection.connect(function(err) {
-//   if (err) {
-//     console.log('Error connecting to Db');
-//     return;
-//   }
-//   console.log('Connection established');
-// });
+const connection = mysql.createConnection ({
+  host: 'localhost',
+  user: 'root',
+  password: 'pw1234',
+  database: 'reddit',
+});
+connection.connect(function(err) {
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -31,6 +31,18 @@ app.get('/hello', (req, res) => {
   res.send("Hello world");
 });
 
+app.get('/posts', (req, res) => {
+  connection.query(`SELECT * FROM posts`, function(err, result) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.json({
+      posts: result,
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on port ${PORT}`);  
