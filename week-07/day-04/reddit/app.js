253c37060cf999882293ec5137e369c6bb9eed12
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-10-01 19:01:28 +0200

    add(reddit):arrow images,new post html,voting methods

--- week-07/day-04/reddit/app.js
+++ week-07/day-04/reddit/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const mysql = require('mysql');
 
+app.use(bodyParser.urlencoded({ extended: false}))
 app.use('/assets', express.static('assets'));
 
 const connection = mysql.createConnection ({
@@ -27,8 +28,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/hello', (req, res) => {
-  res.send("Hello world");
+app.get('/newpost', (req, res) => {
+  res.sendFile(path.join(__dirname, 'new_post.html'));
 });
 
 app.get('/posts', (req, res) => {
@@ -45,36 +46,27 @@ app.get('/posts', (req, res) => {
 });
 
 app.post('/posts', jsonParser, (req, res) => {
-  let title = req.body.title;
+  let title = req.body.title;  
   let url = req.body.url;
-  connection.query(`INSERT INTO posts SET ?`, {"title": title, "url": url}, function(err, result_id) {
+  connection.query(`INSERT INTO posts SET ?`, {"title": title, "url": url}, function(err, result) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
       return;
     }
-    connection.query(`SELECT * FROM posts WHERE id = ${result_id.insertId}`, function(err, result_newPost) {
-      if (err) {
-        console.log(err.toString());
-        res.status(500).send('Database error');
-        return;
-      }
-      res.json(
-        result_newPost[0]
-    );
-    });
+    res.redirect('/');
   });
 });
 
 app.put('/posts/:id/upvote', jsonParser, (req, res) => {
   let id = req.params.id;
-  connection.query(`UPDATE posts SET score=score+1 WHERE id = ${id}`, function(err, result) {
+  connection.query(`UPDATE posts SET score=score+1 WHERE id = ?`, [id], function(err, result) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
       return;
     }
-    connection.query(`SELECT * FROM posts WHERE id = ${id}`, function(err, result_upvotedPost) {
+    connection.query(`SELECT * FROM posts WHERE id = ?`, [id], function(err, result_upvotedPost) {
       if (err) {
         console.log(err.toString());
         res.status(500).send('Database error');
@@ -89,13 +81,13 @@ app.put('/posts/:id/upvote', jsonParser, (req, res) => {
 
 app.put('/posts/:id/downvote', jsonParser, (req, res) => {
   let id = req.params.id;
-  connection.query(`UPDATE posts SET score=score-1 WHERE id = ${id}`, function(err, result) {
+  connection.query(`UPDATE posts SET score=score-1 WHERE id = ?`, [id], function(err, result) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
       return;
     }
-    connection.query(`SELECT * FROM posts WHERE id = ${id}`, function(err, result_downvotedPost) {
+    connection.query(`SELECT * FROM posts WHERE id = ?`, [id], function(err, result_downvotedPost) {
       if (err) {
         console.log(err.toString());
         res.status(500).send('Database error');
@@ -110,7 +102,7 @@ app.put('/posts/:id/downvote', jsonParser, (req, res) => {
 
 app.delete('/posts/:id', (req, res) => {
   let id = req.params.id;
-  connection.query(`DELETE FROM posts WHERE id = ${id}`, function(err, result) {
+  connection.query(`DELETE FROM posts WHERE id = ?`, [id], function(err, result) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
@@ -130,7 +122,7 @@ app.put('/posts/:id', jsonParser, (req, res) => {
       res.status(500).send('Database error');
       return;
     }
-    connection.query(`SELECT * FROM posts WHERE id = ${id}`, function(err, result_modifiedPost) {
+    connection.query(`SELECT * FROM posts WHERE id = ?`, [id], function(err, result_modifiedPost) {
       if (err) {
         console.log(err.toString());
         res.status(500).send('Database error');
--- week-07/day-04/reddit/assets/script.js
+++ week-07/day-04/reddit/assets/script.js
@@ -1,6 +1,15 @@
 window.onload = () => {
   const host = 'http://localhost:8888';
 
+  function votingFetch (id, direction) {
+    fetch(`${host}/posts/${id}/${direction}vote`, {
+      method: 'put', 
+      headers: {
+       "Content-Type": "application/json; charset=utf-8",
+      }
+    });
+  };
+
   fetch(`${host}/posts`)
   .then((resp) => (resp.json()))
   .then(response => {
@@ -13,18 +22,35 @@ window.onload = () => {
       voting.classList.add('voting');
       let content = document.createElement('div');
       content.classList.add('content');
-      for (key in element) {
-        let item = document.createElement('p');
-        item.textContent = element[key];
-        if (key === 'score') {
-          voting.appendChild(item);
-        } else if (key !== 'id') {
-        content.appendChild(item);
-        }        
+      let arrowDown = document.createElement('img');
+      arrowDown.setAttribute('src', './assets/images/downvote.png');
+      arrowDown.addEventListener('click', (e) => {
+        arrowDown.setAttribute('src', './assets/images/downvoted.png');
+        votingFetch(element.id, 'down');
+        e.target.parentElement.childNodes[1].textContent -= 1;
+      });  
+      let arrowUp = document.createElement('img');
+      arrowUp.setAttribute('src', './assets/images/upvote.png');
+      arrowUp.addEventListener('click', (e) => {
+        arrowUp.setAttribute('src', './assets/images/upvoted.png');
+        votingFetch(element.id, 'up');
+        e.target.parentElement.childNodes[1].textContent = Number(e.target.parentElement.childNodes[1].textContent) + 1;
+      }); 
+      voting.appendChild(arrowUp);
+      let score = document.createElement('p');
+      score.textContent = element['score'];
+      voting.appendChild(score);
+      voting.appendChild(arrowDown);
+      let link = document.createElement('a');
+      link.setAttribute('href', element['url']);
+      link.textContent = element['title'];
+      content.appendChild(link);
+      let postInfo = document.createElement('p');
+      postInfo.textContent = 'submitted on ' + element['published'] + ' by ' + element['owner'];
+      content.appendChild(postInfo);
       newPost.appendChild(voting);
       newPost.appendChild(content);
-      }
     list.appendChild(newPost);
     });
   });
-} 
+}; 
