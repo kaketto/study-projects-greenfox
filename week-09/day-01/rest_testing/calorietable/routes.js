f3077c98b8f6b6afd9c840cf1fba5b0195860a7f
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-10-01 16:17:03 +0200

    add endpoints post new item, delete and update, test to fix still

--- week-09/day-01/rest_testing/calorietable/routes.js
+++ week-09/day-01/rest_testing/calorietable/routes.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const connection = mysql.createConnection ({
   host: 'localhost',
@@ -30,4 +32,54 @@ app.get('/drax', (req, res) => {
   })
 });
 
-module.exports = app;
+app.post('/drax/newitem', jsonParser, (req, res) => {
+  let name = req.body.name;
+  let amount_grams = req.body.amount_grams;
+  let calories = req.body.calories;
+  connection.query(`insert into calories set ?`, {"name": name, "amount_grams": amount_grams, "calories": calories}, (err, result) => {
+    if (err) {
+      res.status(500).send('Database error');
+      return;
+    }
+    res.redirect('/drax');
+  })
+})
+
+app.delete('/drax/:name', (req, res) => {
+  let name = req.params.name;
+  connection.query(`DELETE FROM calories WHERE name = ?`, [name], function(err, result) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.status(204).send();
+  });
+});
+
+app.put('/drax/:name', jsonParser, (req, res) => {
+  let name = req.params.name;
+  let newAmount = req.body.amount_grams;
+  let newCalories = req.body.calories;
+
+  connection.query(`UPDATE calories SET amount_grams = ?, calories = ? WHERE name = ?`, [newAmount, newCalories, name], function(err, result) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    connection.query(`SELECT * FROM calories WHERE name = ?`, [name], function(err, result_modifiedPost) {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).send('Database error');
+        return;
+      }
+      res.json(
+        result_modifiedPost[0]
+      );
+    });
+  });
+});
+
+
+module.exports = { app, mysql};
--- week-09/day-01/rest_testing/calorietable/test/test-routes.js
+++ week-09/day-01/rest_testing/calorietable/test/test-routes.js
@@ -5,7 +5,7 @@ const request = require('supertest');
 const routeModule = require('../routes');
 
 test('get list of calories table', (t) => {
-  request(routeModule)
+  request(routeModule.app)
   .get('/drax')
   .expect('Content-Type', /json/)
   .expect(200)
@@ -13,7 +13,37 @@ test('get list of calories table', (t) => {
     if (err) {
       t.error(err)
     }
-    t.notEqual(res.body, []);
+    t.same(res.body[0],  { id: 1, name: 'aubergine', amount_grams: 100, calories: 21 });
   });
   t.end();
+});
+
+test('delete item from calories table', (t) => {
+  request(routeModule.app)
+  .delete('/drax/beetroot')
+  .expect(204)
+  .end( (err, res) => {
+    if (err) {
+      t.error(err)
+    }
+    t.end();
+  });
+});
+
+test('add new item to calories table', (t) => {
+  request(routeModule.app)
+  .post('/drax/newitem')
+  .send({
+    "name": "beetroot",
+    "amount_grams": 100,
+    "calories": 46
+  })
+  .expect(200)
+  .end( (err, res) => {
+    if (err) {
+      t.error(err)
+    }
+    t.same(res.body[3],  { id: 5, name: 'cucumber', amount_grams: 100, calories: 13 });
+    t.end();
+  });
 });
